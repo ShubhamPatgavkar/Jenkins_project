722e14cb303e3bc4a57f6961ec88c5f900a71301
Shubham Patgavkar <shubhampatgavkar5@gmail.com>
2026-07-26 21:35:46 +0530

    Added Shared Library

---
+++ vars/dockerPush.groovy
@@ -0,0 +1,21 @@
+def call(String imageName, String dockerHubRepo) {
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'dockerhub-creds',
+            usernameVariable: 'DOCKER_USERNAME',
+            passwordVariable: 'DOCKER_PASSWORD'
+        )
+    ]) {
+
+        sh """
+            echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
+
+            docker tag ${imageName} \$DOCKER_USERNAME/${dockerHubRepo}:latest
+
+            docker push \$DOCKER_USERNAME/${dockerHubRepo}:latest
+
+            docker logout
+        """
+    }
+}
