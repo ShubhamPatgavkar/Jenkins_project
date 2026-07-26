da7f4becfa26e0626a619b74c213f5865d4da1b3
Shubham Patgavkar <shubhampatgavkar5@gmail.com>
2026-07-26 23:04:21 +0530

    Fixed Docker Push

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,18 +1,23 @@
 def call(String imageName, String dockerHubRepo) {
-        
-         withCredentials([
+
+    withCredentials([
         usernamePassword(
             credentialsId: 'dockerhub-creds',
             usernameVariable: 'DOCKER_USERNAME',
+            passwordVariable: 'DOCKER_PASSWORD'
         )
-    ])
-      {  
+    ]) {
+
         sh """
+            echo "\$DOCKER_PASSWORD" | docker login \
+                -u "\$DOCKER_USERNAME" \
+                --password-stdin
+
             docker tag ${imageName} \$DOCKER_USERNAME/${dockerHubRepo}:latest
 
             docker push \$DOCKER_USERNAME/${dockerHubRepo}:latest
 
             docker logout
         """
-      }
+    }
 }
