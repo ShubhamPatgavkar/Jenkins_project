7a2ca53c69bb92eb831746a272605dfb20971040
Shubham Patgavkar <shubhampatgavkar5@gmail.com>
2026-07-26 22:58:59 +0530

    Fixed DockerPush

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,4 +1,12 @@
 def call(String imageName, String dockerHubRepo) {
+        
+         withCredentials([
+        usernamePassword(
+            credentialsId: 'dockerhub-creds',
+            usernameVariable: 'DOCKER_USERNAME',
+        )
+    ])
+      {  
         sh """
             docker tag ${imageName} \$DOCKER_USERNAME/${dockerHubRepo}:latest
 
@@ -6,5 +14,5 @@ def call(String imageName, String dockerHubRepo) {
 
             docker logout
         """
-
+      }
 }
